9374f1cb904cb4c8ee2908cc60967c5a5609e020
Alex <kleykovsky@gmail.com>
2022-10-02 04:10:12 +0500

    rev: 1.1

--- src/testApp.js
+++ src/testApp.js
@@ -1,26 +1,24 @@
 import logo_apple from './logo_apple.png';
-import './testApp.css';
+import './TestApp.css';
 
 const txt = 'Hello Apple!'
 
-function testApp() {
-    return(
+function TestApp() {
+    return (
         <div className={'title'}>
             <div className="title-display">
-
                 <h1 className={'title-h1'}>{txt}</h1>
                 <img src={logo_apple} className={'title-logo'} alt='logo'/>
-                <text className={'title-text'}>
+                <p className={'title-text'}>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, voluptatem?
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, voluptatem?
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, voluptatem?
-                </text>
+                </p>
             </div>
             <a className={'title-link'} href={'https://apple.com'}>
                 ***Please click***
             </a>
-
         </div>
     );
 }
-export default testApp;
+export default TestApp;
