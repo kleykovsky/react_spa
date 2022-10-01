ffb47bb002aad127a46fa96888af12cd0a451456
Alex <kleykovsky@gmail.com>
2022-10-01 22:32:43 +0500

    rev: 1.1

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import testApp from "./testApp";
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const txt = 'Hello World!'
-const  btn = 'btn'
-const elem = (
-    <div>
-        <h2>{txt}</h2>
-        <input type="text" name="" id=""/>
-        <button className={btn} tabIndex={0}>submit</button>
-    </div>
-);
+// const txt = 'Hello World!'
+// const  btn = 'btn'
+// const elem = (
+//     <div>
+//         <h2>{txt}</h2>
+//         <input type="text" name="" id=""/>
+//         <button className={btn} tabIndex={0}>submit</button>
+//     </div>
+// );
 
 
-// const elem = React.createElement('h2', {className: 'newClass'}, 'Hello world!');
+// const elem = React.createElement(
+//     'h2', {className: 'newClass'}, 'Hello world!',
+// );
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem,
+ // App()
+    testApp()
 );
 
-reportWebVitals();
+// reportWebVitals();
